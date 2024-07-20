67cd11b01582dc0c0c8bb89f90459b7deac51a7b
Cockfla <eliasbaha100@gmail.com>
2024-07-20 00:09:39 -0400

    cambios favicon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          DEFAULT: "#ffffff", // Color para modo oscuro
+        },
+        light: {
+          DEFAULT: "#000000", // Color para modo claro
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      fill: ["dark"], // Añade la variante 'dark' para fill
+    },
+  },
+  plugins: [],
+};
